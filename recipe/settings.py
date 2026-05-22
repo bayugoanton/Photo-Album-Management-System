480fe45ea47f3d134e4b2912658a2fdcbb95981a
Anton Bayugo <bayugo.anton@gmai.com>
2026-05-23 03:03:44 +0800

    commit

--- recipe/settings.py
+++ recipe/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # --- YOUR APP MUST BE HERE ---
-    'gallery', 
+    'Gallery', 
     
     'cloudinary',
     'cloudinary_storage',
